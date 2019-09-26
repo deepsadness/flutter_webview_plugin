ea129124cd839a2a328a53a6b6a1d532cb05eaad
deepsadness <a295756092@gmail.com>
2019-09-26 21:28:15 +0800

    添加键盘的适配

--- android/src/main/java/com/flutter_webview_plugin/FlutterWebviewPlugin.java
+++ android/src/main/java/com/flutter_webview_plugin/FlutterWebviewPlugin.java
@@ -6,6 +6,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.graphics.Point;
 import android.view.Display;
+import android.view.WindowManager;
 import android.widget.FrameLayout;
 import android.webkit.CookieManager;
 import android.webkit.ValueCallback;
@@ -119,6 +120,8 @@ public class FlutterWebviewPlugin implements MethodCallHandler, PluginRegistry.A
         FrameLayout.LayoutParams params = buildLayoutParams(call);
 
         activity.addContentView(webViewManager.webView, params);
+        activity.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE|
+                WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);
 
         webViewManager.openUrl(withJavascript,
                 clearCache,
